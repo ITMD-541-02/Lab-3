93fa204fda08f80eea66199fad2d7c8b325e4d53
Philip Le <lephil505006@gmail.com>
2022-10-22 15:07:19 -0500

    Finished javascript portion

--- js/site.js
+++ js/site.js
@@ -1,11 +1,17 @@
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
+
 output.innerHTML = slider.value; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function () {
     output.innerHTML = this.value;
-}
+    document.getElementById("percentage").value = output.innerHTML + "%";
 
-const bill = document.getElementById('Bill');
-const grandTotal = document.getElementById('total');
+    var tip = output.innerHTML * .01;
+    var amount = (bill.value * tip).toFixed(2);
+    var grandTotal = (parseFloat(bill.value) + parseFloat(amount)).toFixed(2);
+
+    document.getElementById("amount").value = "$" + amount;
+    document.getElementById("total").value = "$" + grandTotal;
+}
